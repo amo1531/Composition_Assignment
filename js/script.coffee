0a11e8bc961f2203c4f73072e167e0c1d6b13db6
Akshay Mahajan <akshay.mahajan@springernature.com>
2018-01-21 16:34:10 +0530

    Abhishek:- Generic CS code for the composition section.

--- js/script.coffee
+++ js/script.coffee
@@ -5,65 +5,127 @@ $(document).ready ->
 	c.beginPath()
 	c.lineWidth = 0.2
 	c.moveTo(136.5,58)
-	c.lineTo(250.5,20)
+	c.lineTo(260,20)
 	c.lineTo(300.5,20)
 	c.stroke()
 
 	c.moveTo(186.5,67)
-	c.lineTo(258.5,40)
+	c.lineTo(260,40)
 	c.lineTo(308.5,40)
 	c.stroke()
 
 	c.moveTo(210.5,75)
-	c.lineTo(264.5,58)
+	c.lineTo(260,58)
 	c.lineTo(314.5,58)
 	c.stroke()
 
 	c.moveTo(216.5,89)
-	c.lineTo(274.5,77)
+	c.lineTo(260,77)
 	c.lineTo(324.5,77)
 	c.stroke()
 
 	
 	$('#firstComponent').addClass('componentDetails--disable')
 
-	$('#itemOne').on "click", () ->
-		$('.composition').find('.componentDetails').addClass("componentDetails--disable")
-		$('.composition').find('#firstComponentDiv').removeClass("componentDetails--disable")
-		$('.lable_list li').removeClass("active")
-		$(this).addClass("active")
+	$('.lable_listItem').on "click", () -> 
+		$('.lable_listItem').removeClass 'active'
+		$('.composition_imageLink').removeClass 'active'
+		activePoint = $(this).attr('pivotPoint')
+		pivotPointList = $('.composition_image').find('.composition_imageLink')
 
-	$('#itemTwo').on "click", () ->
-		$('.composition').find('.componentDetails').addClass("componentDetails--disable")
-		$('.composition').find('#secondComponentDiv').removeClass("componentDetails--disable")
-		$('.lable_list li').removeClass("active")
-		$(this).addClass("active");
+		for elements in pivotPointList
+			if activePoint == $(elements).attr("pivotPoint")
+				$(elements).addClass('active')
+				$(this).toggleClass('active')
 
-	$('#itemThree').on "click", () ->
-		$('.composition').find('.componentDetails').addClass("componentDetails--disable")
-		$('.composition').find('#thirdComponentDiv').removeClass("componentDetails--disable")
-		$('.lable_list li').removeClass("active")
-		$(this).addClass("active")
+		compositionSec=$('.componentDetails')
+		for compElement in compositionSec
+			if activePoint == $(compElement).attr('relPoint')
+				$(compElement).removeClass("componentDetails--disable")
+			else	
+				$(compElement).addClass("componentDetails--disable")
+
+		# $('.composition_imageLink').each(()-> {
+		# 	point = $(this).attr('pivotPoint')
+
+		# 	})
+		# console.log point
+
+		# $(this).toggleClass 'active'
 		
-	$('#itemFour').on "click", () ->
-		$('.composition').find('.componentDetails').addClass("componentDetails--disable")
-		$('.composition').find('#fourthComponentDiv').removeClass("componentDetails--disable")
-		$('.lable_list li').removeClass("active")
-		$(this).addClass("active")
+		# i = 0
+		# while i < pivotPointList.length
+		# 	pivotPointList[i].attr('pivotpoint') == pivotPoint
+		# 	console.log(pivotPointList[i].addClass('active'))
+		# 	i++
+		# $.each(pivotPointList , function(element){
+		# 	console.log element.attr('pivotPoint')
+		# 	})
+
+
+
+	# $('#itemOne').on "click", () ->
+	# 	$('.composition').find('.componentDetails').addClass("componentDetails--disable")
+	# 	$('.composition').find('#firstComponentDiv').removeClass("componentDetails--disable")
+	# 	$('.lable_list li').removeClass("active")
+	# 	$(this).addClass("active")
+
+	# $('#itemTwo').on "click", () ->
+	# 	$('.composition').find('.componentDetails').addClass("componentDetails--disable")
+	# 	$('.composition').find('#secondComponentDiv').removeClass("componentDetails--disable")
+	# 	$('.lable_list li').removeClass("active")
+	# 	$(this).addClass("active");
+
+	# $('#itemThree').on "click", () ->
+	# 	$('.composition').find('.componentDetails').addClass("componentDetails--disable")
+	# 	$('.composition').find('#thirdComponentDiv').removeClass("componentDetails--disable")
+	# 	$('.lable_list li').removeClass("active")
+	# 	$(this).addClass("active")
+		
+	# $('#itemFour').on "click", () ->
+	# 	$('.composition').find('.componentDetails').addClass("componentDetails--disable")
+	# 	$('.composition').find('#fourthComponentDiv').removeClass("componentDetails--disable")
+	# 	$('.lable_list li').removeClass("active")
+	# 	$(this).addClass("active")
 
 
 	$('.crossButton').on 'click', (e) ->
 		e.preventDefault()
-		$('#firstComponent').removeClass('componentDetails--disable')
+		# $('#firstComponent').removeClass('componentDetails--disable')
 		$(this).parent().parent().addClass('componentDetails--disable')
 		$(this).parent().find(".componentDetails_wrapper").css({"display":"none"})
 		$(this).css({"display":"none"})
+
+		pivotPointList = $('.composition_image').find('.composition_imageLink')
+		for elements in pivotPointList
+			if $(this).parent().parent().attr('relPoint') == $(elements).attr('pivotPoint')
+				$(elements).removeClass('active')
+
+		labelList = $('.composition_image').find('.lable_listItem')
+		for elements in labelList
+			if $(this).parent().parent().attr('relPoint') == $(elements).attr('pivotPoint')
+				$(elements).removeClass('active')		
+
 
 	$(".componentsHeading h3").on "click", () ->
 		$(this).parent().parent().removeClass("componentDetails--disable")
 		$(this).parent().parent().siblings('div').addClass("componentDetails--disable")
 		$(this).siblings('.crossButton').css({"display":"block"})
 		$(this).parent().parent().siblings('div').find('a').css({"display":"none"})
+
+		pivotPointList = $('.composition_image').find('.composition_imageLink')
+		for elements in pivotPointList
+			if $(this).parent().parent().attr('relPoint') == $(elements).attr('pivotPoint')
+				$(elements).addClass('active')
+			else	
+				$(elements).removeClass('active')
+
+		labelList = $('.composition_image').find('.lable_listItem')
+		for elements in labelList
+			if $(this).parent().parent().attr('relPoint') == $(elements).attr('pivotPoint')
+				$(elements).addClass('active')	
+			else		
+				$(elements).removeClass('active')	
 
 
 
--- js/script.js
+++ js/script.js
@@ -7,66 +7,106 @@
     c.beginPath();
     c.lineWidth = 0.2;
     c.moveTo(136.5, 58);
-    c.lineTo(250.5, 20);
+    c.lineTo(260, 20);
     c.lineTo(300.5, 20);
     c.stroke();
     c.moveTo(186.5, 67);
-    c.lineTo(258.5, 40);
+    c.lineTo(260, 40);
     c.lineTo(308.5, 40);
     c.stroke();
     c.moveTo(210.5, 75);
-    c.lineTo(264.5, 58);
+    c.lineTo(260, 58);
     c.lineTo(314.5, 58);
     c.stroke();
     c.moveTo(216.5, 89);
-    c.lineTo(274.5, 77);
+    c.lineTo(260, 77);
     c.lineTo(324.5, 77);
     c.stroke();
     $('#firstComponent').addClass('componentDetails--disable');
-    $('#itemOne').on("click", function() {
-      $('.composition').find('.componentDetails').addClass("componentDetails--disable");
-      $('.composition').find('#firstComponentDiv').removeClass("componentDetails--disable");
-      $('.lable_list li').removeClass("active");
-      return $(this).addClass("active");
-    });
-    $('#itemTwo').on("click", function() {
-      $('.composition').find('.componentDetails').addClass("componentDetails--disable");
-      $('.composition').find('#secondComponentDiv').removeClass("componentDetails--disable");
-      $('.lable_list li').removeClass("active");
-      return $(this).addClass("active");
-    });
-    $('#itemThree').on("click", function() {
-      $('.composition').find('.componentDetails').addClass("componentDetails--disable");
-      $('.composition').find('#thirdComponentDiv').removeClass("componentDetails--disable");
-      $('.lable_list li').removeClass("active");
-      return $(this).addClass("active");
-    });
-    $('#itemFour').on("click", function() {
-      $('.composition').find('.componentDetails').addClass("componentDetails--disable");
-      $('.composition').find('#fourthComponentDiv').removeClass("componentDetails--disable");
-      $('.lable_list li').removeClass("active");
-      return $(this).addClass("active");
+    $('.lable_listItem').on("click", function() {
+      var activePoint, compElement, compositionSec, elements, i, j, len, len1, pivotPointList, results;
+      $('.lable_listItem').removeClass('active');
+      $('.composition_imageLink').removeClass('active');
+      activePoint = $(this).attr('pivotPoint');
+      pivotPointList = $('.composition_image').find('.composition_imageLink');
+      for (i = 0, len = pivotPointList.length; i < len; i++) {
+        elements = pivotPointList[i];
+        if (activePoint === $(elements).attr("pivotPoint")) {
+          $(elements).addClass('active');
+          $(this).toggleClass('active');
+        }
+      }
+      compositionSec = $('.componentDetails');
+      results = [];
+      for (j = 0, len1 = compositionSec.length; j < len1; j++) {
+        compElement = compositionSec[j];
+        if (activePoint === $(compElement).attr('relPoint')) {
+          results.push($(compElement).removeClass("componentDetails--disable"));
+        } else {
+          results.push($(compElement).addClass("componentDetails--disable"));
+        }
+      }
+      return results;
     });
     $('.crossButton').on('click', function(e) {
+      var elements, i, j, labelList, len, len1, pivotPointList, results;
       e.preventDefault();
-      $('#firstComponent').removeClass('componentDetails--disable');
       $(this).parent().parent().addClass('componentDetails--disable');
       $(this).parent().find(".componentDetails_wrapper").css({
         "display": "none"
       });
-      return $(this).css({
+      $(this).css({
         "display": "none"
       });
+      pivotPointList = $('.composition_image').find('.composition_imageLink');
+      for (i = 0, len = pivotPointList.length; i < len; i++) {
+        elements = pivotPointList[i];
+        if ($(this).parent().parent().attr('relPoint') === $(elements).attr('pivotPoint')) {
+          $(elements).removeClass('active');
+        }
+      }
+      labelList = $('.composition_image').find('.lable_listItem');
+      results = [];
+      for (j = 0, len1 = labelList.length; j < len1; j++) {
+        elements = labelList[j];
+        if ($(this).parent().parent().attr('relPoint') === $(elements).attr('pivotPoint')) {
+          results.push($(elements).removeClass('active'));
+        } else {
+          results.push(void 0);
+        }
+      }
+      return results;
     });
     return $(".componentsHeading h3").on("click", function() {
+      var elements, i, j, labelList, len, len1, pivotPointList, results;
       $(this).parent().parent().removeClass("componentDetails--disable");
       $(this).parent().parent().siblings('div').addClass("componentDetails--disable");
       $(this).siblings('.crossButton').css({
         "display": "block"
       });
-      return $(this).parent().parent().siblings('div').find('a').css({
+      $(this).parent().parent().siblings('div').find('a').css({
         "display": "none"
       });
+      pivotPointList = $('.composition_image').find('.composition_imageLink');
+      for (i = 0, len = pivotPointList.length; i < len; i++) {
+        elements = pivotPointList[i];
+        if ($(this).parent().parent().attr('relPoint') === $(elements).attr('pivotPoint')) {
+          $(elements).addClass('active');
+        } else {
+          $(elements).removeClass('active');
+        }
+      }
+      labelList = $('.composition_image').find('.lable_listItem');
+      results = [];
+      for (j = 0, len1 = labelList.length; j < len1; j++) {
+        elements = labelList[j];
+        if ($(this).parent().parent().attr('relPoint') === $(elements).attr('pivotPoint')) {
+          results.push($(elements).addClass('active'));
+        } else {
+          results.push($(elements).removeClass('active'));
+        }
+      }
+      return results;
     });
   });
 
